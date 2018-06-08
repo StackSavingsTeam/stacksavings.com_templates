bd0c5ba354c8ef8d7009761be7acbd79b9f19fdf
David <dmolina101@gmail.com>
2018-06-07 21:32:59 -0400

    Update

--- layouts/360-bootstrap/assets/360-bootstrap/index.js
+++ layouts/360-bootstrap/assets/360-bootstrap/index.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
     "autoLoad" : true,
     "autoRotate": -2,
     "type": "equirectangular",
-    "panorama": "assets/360-bootstrap/alma.jpg",
+    "panorama": "assets/360-bootstrap/kitchen2.jpg",
     "showControls": false
   });
 
