a8d30436729ee88f8b6f0372f5183acb0fad1c68
David <dmolina101@gmail.com>
2018-06-11 13:19:10 -0400

    Update 360°

--- layouts/themeForestResponsive18Augst/html/js/custom.js
+++ layouts/themeForestResponsive18Augst/html/js/custom.js
@@ -1,5 +1,14 @@
 jQuery(document).ready(function($) {
 
+	pannellum.viewer('panorama', {
+    "autoLoad" : true,
+    "autoRotate": -2,
+    "type": "equirectangular",
+    "panorama": "img/badroom.jpg",
+    "showControls": false
+  });
+
+
 	/*
 	Dropdown
 	=========================== */
@@ -8,19 +17,19 @@ jQuery(document).ready(function($) {
 		}, function () {
 			$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
 	});
-	
+
 	/* Browse menu
-	=========================== */	
+	=========================== */
 	$("#browse-menu").hide();
 
 		$('.open-menu a').click(function(){
 			$("#browse-menu").slideToggle();
-		return false; 
+		return false;
 	});
-		
+
 	/*
 	Bounce animated
-	=========================== */	
+	=========================== */
 	$(".e_bounce").hover(
 		function () {
 		$(this).addClass("animated bounce");
@@ -29,10 +38,10 @@ jQuery(document).ready(function($) {
 		$(this).removeClass("animated bounce");
 		}
 	);
-	
+
 	/*
 	image hover
-	=========================== */	
+	=========================== */
 	$(".zoom, .image-title").css({'opacity':'0','filter':'alpha(opacity=0)'});
    jQuery('.image-wrapper').mouseenter(function(e) {
 	    jQuery(this).children('.image-caption').show().css('opacity', '1').stop().animate({top: '0', opacity : '1'}, 600);
@@ -44,15 +53,15 @@ jQuery(document).ready(function($) {
 		jQuery(this).children('.image-title').stop().animate({top: '-65px', opacity : '0'}, 600);
 		jQuery(this).children('.zoom').show().css('opacity', '0').stop().animate({opacity : '0'}, 1000);
 		jQuery(this).children('.portfolio-metta').stop().animate({bottom: '-24px', opacity : '0'}, 600);
-    });	
+    });
 
 	/*
 	team hover
-	=========================== */	
+	=========================== */
 	$('.team-wrapper').hover(function(){
 		$(".team-img", this).stop().animate({top:'-100%'},{queue:false,duration:550});
 	}, function() {
 		$(".team-img", this).stop().animate({top:'0'},{queue:false,duration:550});
-	});	
-	
-})(jQuery);	
+	});
+
+})(jQuery);
